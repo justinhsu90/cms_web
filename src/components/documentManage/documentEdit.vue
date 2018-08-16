06ac1d7295f5017de0f7086b62be021303d9737c
huwei <337502348@qq.com>
2018-08-16 20:57:12 +0800

    no message

--- src/components/documentManage/documentEdit.vue
+++ src/components/documentManage/documentEdit.vue
@@ -713,6 +713,14 @@ export default {
       let ReplaceWordKey1 = this.data.ReplaceWordKey1;
       let ReplaceWordKey2 = this.data.ReplaceWordKey2;
       let ReplaceWordKey3 = this.data.ReplaceWordKey3;
+
+      ReplaceWordValue1 = ReplaceWordValue1.replace(/\[/g,'\\[');
+      ReplaceWordValue1 = ReplaceWordValue1.replace(/\]/g,'\\]');
+      ReplaceWordValue2 = ReplaceWordValue2.replace(/\[/g,'\\[');
+      ReplaceWordValue2 = ReplaceWordValue2.replace(/\]/g,'\\]');
+      ReplaceWordValue3 = ReplaceWordValue3.replace(/\[/g,'\\[');
+      ReplaceWordValue3 = ReplaceWordValue3.replace(/\]/g,'\\]'); 
+
        if(!!ReplaceWordValue1){
         let reg = new RegExp(`${ReplaceWordValue1}`,'g');
         totalDescription = totalDescription.replace(reg,ReplaceWordKey1);
