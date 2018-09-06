5e908c2c023ce6a43f0191039a4d5e66f101236e
huwei <337502348@qq.com>
2018-09-06 23:04:19 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import ElementUI from '../static/lib/ele/index';
 import 'element-ui/lib/theme-chalk/index.css';
 import "./assets/css/reset.css"
 import "./assets/css/fontStyle.css";
-import Cookies from "../node_modules/js-cookie/src/index"
+// import Cookies from "../node_modules/js-cookie/src/index"
 import App from './App'
 import moment from 'moment'
 import axios from '@/common/until/Ajax'
