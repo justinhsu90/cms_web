52f148a8f5f51f9aaca93a0d6dad5143ed7d4d06
huwei <337502348@qq.com>
2018-10-20 13:02:33 +0800

    no message

--- src/common/wonTableContainer.vue
+++ src/common/wonTableContainer.vue
@@ -96,7 +96,17 @@ export default {
                 this.fetchCondition.order = row.prop;
             }
             this.handleSearch();
-        }
+        },
+        saveFile(url, filename) {
+            var save_link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a')
+            save_link.href = url;
+            save_link.download = filename;
+            save_link.target = "_blank";
+
+            var event = document.createEvent('MouseEvents');
+            event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            save_link.dispatchEvent(event);
+        },
         // fetchTableData() {
         //   this.isTableLoading = true;
         //   axios({
--- src/components/excelUpload/excelUploadList.vue
+++ src/components/excelUpload/excelUploadList.vue
@@ -1,34 +1,56 @@
 <template>
-  <div>
-    <el-row>
-      <el-col :span="22">
-        <el-input placeholder="搜索" v-model="fetchOption.where" @keyup.enter.native="handleSearch" style="width:22%;float:left">
-        </el-input>
-        <div style="margin-left:5px;display:inline-block;width:140px">
-          <el-select placeholder="file" v-model="searchAccount" @change="handleCondition('acc')" clearable>
-            <el-option v-for="(v,i) in searchAccountOption" :key="'acc'+i" :label="v.account" :value="v.account"></el-option>
-          </el-select>
-        </div>
-        <div style="cursor:pointer;display: inline-block;width: 14px;height: 35px;text-align: center;border: 1px solid #dcdfe6;border-radius: 4px;line-height: 35px;" @click="handleSearch" class="el-input-group__append">
-          <i class="el-icon-search"></i>
-        </div>
-      </el-col>
-      <el-col :span="2">
+    <div>
+        <el-row>
+            <el-col :span="22">
+                <el-input placeholder="搜索" v-model="fetchOption.where" @keyup.enter.native="handleSearch" style="width:22%;float:left">
+                </el-input>
+                <div style="margin-left:5px;display:inline-block;width:140px">
+                    <el-select placeholder="file" v-model="searchFileType" @change="handleCondition('file')" clearable>
+                        <el-option v-for="(v,i) in searchFiletypeOption" :key="'acc'+i" :label="v" :value="v"></el-option>
+                    </el-select>
+                </div>
+                <div style="cursor:pointer;display: inline-block;width: 14px;height: 35px;text-align: center;border: 1px solid #dcdfe6;border-radius: 4px;line-height: 35px;" @click="handleSearch" class="el-input-group__append">
+                    <i class="el-icon-search"></i>
+                </div>
+            </el-col>
+            <el-col :span="2">
                 <el-button style="float:right" type="primary" @click="handleUpload" size="small">上传文件</el-button>
-        </el-col>
-      <el-col class="mt5">
-        <el-table ref="wonTable" :max-height="maxHeight" :data="tableData" v-loading="isTableLoading" @sort-change="handleSortChange">
-          <el-table-column min-width="80" label="Wowcher Code" prop="wowcherCode">
-          </el-table-column>
+            </el-col>
+            <el-col class="mt5">
+                <el-table ref="wonTable" :max-height="maxHeight" :data="tableData" v-loading="isTableLoading" @sort-change="handleSortChange">
+                    <el-table-column min-width="80" label="fileId" prop="fileId">
+                    </el-table-column>
+                    <el-table-column min-width="100" label="fileName" prop="fileName">
+                    </el-table-column>
+                    <el-table-column min-width="80" label="fileType" prop="fileType">
+                    </el-table-column>
 
-        </el-table>
-      </el-col>
-      <div style="float:right;margin-top:5px">
-        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :total='total' :current-page="currentPage" :page-sizes="pageSizes" :layout="layout">
-        </el-pagination>
-      </div>
-    </el-row>
-  </div>
+                    <el-table-column min-width="80" label="addedBy" prop="addedBy">
+                    </el-table-column>
+                    <!-- <el-table-column min-width="80" label="fileUrl" prop="fileUrl">
+              <template slot-scope="scope">
+                  <img :src="scope.row.fileUrl" alt="" style="width:80px">
+              </template>
+          </el-table-column> -->
+                    <el-table-column min-width="80" label="uploadStatus" prop="uploadStatus">
+                    </el-table-column>
+                    <el-table-column min-width="80" label="addedTime" prop="addedTime" :formatter="formatToTime">
+                    </el-table-column>
+                    <el-table-column min-width="80" label="lastUpdatedTime" prop="lastUpdatedTime" :formatter="formatToTime">
+                    </el-table-column>
+                    <el-table-column width="80" label="動作" fixed="right" align="center">
+                        <template slot-scope="scope">
+                            <el-button class="btnh" type="text" title="下载" icon="el-icon-won-102" @click="handleDown(scope.row)"></el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-col>
+            <div style="float:right;margin-top:5px">
+                <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :total='total' :current-page="currentPage" :page-sizes="pageSizes" :layout="layout">
+                </el-pagination>
+            </div>
+        </el-row>
+    </div>
 </template>
 <script>
 import wonTableContainer from "@/common/wonTableContainer";
@@ -38,14 +60,8 @@ export default {
         return {
             tableData: [],
             condition: [],
-            searchAccount: "",
-            searchAccountOption: [],
-            searchCountry: "",
-            searchCountryOption: [],
-            searchOrderstatus: "",
-            searchOrderstatusOption: [],
-            searchOrdertype: "",
-            searchOrdertypeOption: [],
+            searchFileType: "",
+            searchFiletypeOption: [],
             maxHeight: 450,
             isTableLoading: false,
             fetchCondition: {
@@ -67,21 +83,19 @@ export default {
                 token: this.token
             }
         });
-        Promise.all([filetype]).then(
-            ([filetype]) => {
-                this.searchFiletypeOption = _.cloneDeep(filetype.data);
-            }
-        );
+        Promise.all([filetype]).then(([filetype]) => {
+            this.searchFiletypeOption = _.cloneDeep(filetype);
+        });
         this.handleSearch();
     },
     methods: {
         handleCondition(sign) {
             if (sign == "file") {
-                if (!this.searchAccount) {
+                if (!this.searchFileType) {
                     _.pull(this.condition, "1");
                 } else {
-                    if(!this.condition.includes('1')){
-                       this.condition.push("1");
+                    if (!this.condition.includes("1")) {
+                        this.condition.push("1");
                     }
                 }
             }
@@ -93,19 +107,10 @@ export default {
                 where: this.fetchOption.where,
                 token: this.token,
                 skip: this.fetchCondition.skip,
-                limit: this.fetchCondition.limit,
+                limit: this.fetchCondition.limit
             };
             if (this.condition.includes("1")) {
-                data.account = this.searchAccount;
-            }
-            if (this.condition.includes("2")) {
-                data.orderstatus = this.searchOrderstatus;
-            }
-            if (this.condition.includes("3")) {
-                data.country = this.searchCountry;
-            }
-            if (this.condition.includes("4")) {
-                data.ordertype = this.searchOrdertype;
+                data.type = this.searchFileType;
             }
             axios({
                 url: this.fetchOption.url,
@@ -117,10 +122,13 @@ export default {
                 this.total = count;
             });
         }, 500),
-        handleUpload(){
+        handleUpload() {
             this.$router.push({
-                name:'addUpload'
-            })
+                name: "addUpload"
+            });
+        },
+        handleDown(row) {
+           this.saveFile(row.fileUrl,row.fileName)
         }
     }
 };
