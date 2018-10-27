b6e8dfd5233abe888d41fd4bf051b0ece4088093
huwei <337502348@qq.com>
2018-10-27 14:46:33 +0800

    no message

--- src/components/excelUpload/excelUploadFile.vue
+++ src/components/excelUpload/excelUploadFile.vue
@@ -7,14 +7,21 @@
             </div>
             <br>
             <el-row>
-                <el-form ref="form" :model="form" label-position="top">
+                <el-form ref="form" :model="form" label-position="right">
                     <el-form-item label="上傳文件">
                         <template slot="label">
                             <span>上傳文件</span>
                             <span class="f-12 label-tips">可以選擇多個文件</span>
                         </template>
                         <el-button size="small" type="success" @click="handleUpload">點擊上傳</el-button>
-                        <div  style="width:80%;margin-top:10px">
+                    </el-form-item>
+                    <el-form-item label="檔案类型全选">
+                        <el-select placeholder="檔案类型" v-model="typeAll" clearable @change="handleSelectAll">
+                            <el-option v-for="(v,i) in searchFiletypeOption" :key="'acc'+i" :label="v" :value="v"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item>
+                        <div style="width:80%;margin-top:10px">
                             <el-table :data="files">
                                 <el-table-column label="文件名" min-width="100" prop="name"></el-table-column>
                                 <el-table-column label="檔案大小" prop="size" width="200">
@@ -24,12 +31,16 @@
                                 </el-table-column>
                                 <el-table-column label="檔案类型" prop="size" width="200">
                                     <template slot-scope="scope">
-                                        <el-select placeholder="檔案类型" v-model="type[scope.$index]" clearable>
-                                            <el-option v-for="(v,i) in searchFiletypeOption" :key="'acc'+i" :label="v" :value="v"></el-option>
-                                        </el-select>
+                                        <el-form v-for="v in [1]" ref="formChild" :model="type" :key="v">
+                                            <el-form-item :prop="scope.$index + ''" :rules="{required:true,message:'此项必填'}">
+                                                <el-select placeholder="檔案类型" v-model="type[scope.$index]" clearable>
+                                                    <el-option v-for="(v,i) in searchFiletypeOption" :key="'acc'+i" :label="v" :value="v"></el-option>
+                                                </el-select>
+                                            </el-form-item>
+                                        </el-form>
                                     </template>
                                 </el-table-column>
-                                <el-table-column width="80" label="操作" fixed="right" align="center">
+                                <el-table-column width="80" label="操作"  align="center">
                                     <template slot-scope="scope">
                                         <el-button class="btnh" type="text" title="下載" icon="el-icon-won-22" @click="handleDelete(scope)"></el-button>
                                     </template>
@@ -46,22 +57,24 @@
 </template>
 
 <script>
-import U from '@/common/until/until'
+import U from "@/common/until/until";
 export default {
     data() {
         return {
-            Div:U.Math.Div,
-            Mul:U.Math.Mul,
+            Div: U.Math.Div,
+            Mul: U.Math.Mul,
             form: {},
+            formChild: {},
             files: [],
             isLoading: false,
-            isEmpty:_.isEmpty,
-            searchFiletypeOption:[],
-            type:{},
+            isEmpty: _.isEmpty,
+            searchFiletypeOption: [],
+            type: {},
+            typeAll: ""
         };
-    },    
-    created(){
-          let filetype = axios({
+    },
+    created() {
+        let filetype = axios({
             url: "/excel/upload/value/filetype",
             method: "post",
             data: {
@@ -76,20 +89,25 @@ export default {
         goBack() {
             this.$router.go(-1);
         },
+        handleSelectAll(value) {
+            _.each(this.type, (v, k) => {
+                this.type[k] = value;
+            });
+        },
         handleUpload() {
             let input = document.createElement("input");
             input.type = "file";
             input.multiple = "multiple";
             input.click();
             input.addEventListener("change", () => {
-                _.each(input.files, (v,i) => {
+                _.each(input.files, (v, i) => {
                     this.files.push(v);
                 });
-                _.each(this.files,(value,index)=>{
-                    if(this.type[index] == ''){
-                        this.$set(this.type,index,'');
+                _.each(this.files, (value, index) => {
+                    if (!this.type[index]) {
+                        this.$set(this.type, index, "");
                     }
-                })
+                });
             });
         },
         handleDelete(scope) {
@@ -97,18 +115,19 @@ export default {
 
             delete this.type[scope.$index];
             let index = 0;
-            let obj = {
-                    
-            }
-            _.each(this.type,(v,k)=>{
-                 obj[index] = v;
-                 index++;
-            })
+            let obj = {};
+            _.each(this.type, (v, k) => {
+                obj[index] = v;
+                index++;
+            });
             this.type = obj;
         },
         submit() {
-            let totalAjax = [];
-            _.each(this.files, (v,i) => {
+            _.each(this.$refs['formChild'],(v)=>{
+                v.validate((valid)=>{
+                    if(valid){
+                          let totalAjax = [];
+            _.each(this.files, (v, i) => {
                 let formData = new FormData();
                 formData.append("token", this.token);
                 formData.append("uploadfile", v);
@@ -131,12 +150,17 @@ export default {
             }
             Promise.all(totalAjax)
                 .then(() => {
-                    this.goBack();
+                    this.$router.push('excelUpload');
                     this.$message.success("保存成功");
                 })
                 .catch(() => {
                     this.$message.error("保存失敗");
                 });
+                    }else{
+
+                    }
+                })
+            })
         }
     }
 };
@@ -157,6 +181,10 @@ export default {
 }
 .f-12 {
     font-size: 12px;
+}
+/deep/ .el-form-item .el-form-item{
+    margin-bottom: 18px;
+    margin-top: 16px;
 }
 </style>
 
