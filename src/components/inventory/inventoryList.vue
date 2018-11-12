33b3a561cdc674025094b08132c8191cf7282039
Justin Hsu <justin.h@mig.me>
2018-11-12 08:15:33 +0800

    update UI

--- src/components/inventory/inventoryList.vue
+++ src/components/inventory/inventoryList.vue
@@ -26,9 +26,9 @@
             <el-col class="mt5">
                 <el-table ref="wonTable" :max-height="maxHeight" :data="tableData" v-loading="isTableLoading" @sort-change="handleSortChange">
                     <el-table-column width="50" label="單號" prop="inventoryChangeId"></el-table-column>
-                    <el-table-column width="60" label="類型" prop="inventoryType"></el-table-column>
+                    <el-table-column width="70" label="類型" prop="inventoryType"></el-table-column>
                     <el-table-column min-width="60" label="SKU" prop="sku"></el-table-column>
-                    <el-table-column min-width="80" label="所屬倉庫" prop="warehouse"></el-table-column>
+                    <el-table-column min-width="70" label="所屬倉庫" prop="warehouse"></el-table-column>
                     <el-table-column width="60" label="數量" prop="quantity"></el-table-column>
                     <el-table-column width="200" label="產品名稱" prop="productName"></el-table-column>
                     <el-table-column width="80" label="帳號" prop="account"></el-table-column>
