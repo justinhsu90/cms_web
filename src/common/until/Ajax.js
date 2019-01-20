76ecec1feb9f97adc8124090e4ae350e649af69a
huwei <337502348@qq.com>
2019-01-20 20:01:45 +0800

    no message

--- src/common/until/Ajax.js
+++ src/common/until/Ajax.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import qs from "qs";
 import C from "js-cookie";
+import Vue from "vue";
 //axios的配置
 // axios.defaults.baseURL = 'http://118.163.10.109:8000/data-server/';
 axios.defaults.baseURL = "http://60.251.57.138:8000/data-server/";
 // axios.defaults.baseURL = 'http://127.0.0.1:8080/data-server/';
+axios.defaults.timeout = 5000;
 axios.interceptors.request.use(request => {
   if (request.isFormData) {
     return request;
@@ -12,16 +14,23 @@ axios.interceptors.request.use(request => {
   request.data = qs.stringify(request.data);
   return request;
 });
-axios.interceptors.response.use(response => {
-  if (response.data.message == "Token is expired") {
-    C.remove("name");
-    C.remove("userName");
-    C.remove("privilege");
-    C.remove("token");
-    location.href = "/";
+axios.interceptors.response.use(
+  response => {
+    if (response.data.message == "Token is expired") {
+      C.remove("name");
+      C.remove("userName");
+      C.remove("privilege");
+      C.remove("token");
+      location.href = "/";
+    }
+    return response.data;
+  },
+  error => {
+    if (error.message.search("timeout") != -1) {
+      Vue.prototype.$message.error("请求链接超时,请重新进行操作");
+    }
   }
-  return response.data;
-});
+);
 function Ajax(config = {}) {
   if (_.isEmpty(config)) {
     Promise.reject("请输入ajax配置");
