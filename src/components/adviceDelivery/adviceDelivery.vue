697482e06df64f8f39bbf34724589c068f3b3964
huwei <337502348@qq.com>
2019-10-20 10:02:34 +0800

    到貨通知

---
+++ src/components/adviceDelivery/adviceDelivery.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <el-form
+      :model="form"
+      ref="form"
+      :rules="rules"
+    >
+      <el-row>
+        <el-col :span="12">
+          <el-form-item
+            label="信息："
+            prop="trackingNumberList"
+          >
+            <el-input
+              rows="6"
+              type="textarea"
+              v-model="form.trackingNumberList"
+            ></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-form-item>
+        <el-button
+          @click="submit"
+          :loading="btnLoading"
+          type="success"
+          size="small"
+        >
+          發送
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      btnLoading: false,
+      form: {
+        trackingNumberList: ''
+      },
+    }
+  },
+  created(){
+    this.rules = {
+      trackingNumberList: {
+        required: true,
+        message: '此項必填'
+      }
+    }
+  },
+  methods: {
+    getValue(){
+      return {
+        ...this.form
+      }
+    },
+    submit(){
+       this.$refs["form"].validate(valid => {
+        if (valid) {
+          this.btnLoading = true;
+          axios({
+            url: "/wowcher/mail/add",
+            method: "post",
+            data: this.getValue()
+          }).then(
+            res => {
+              this.$message.success("提交成功");
+            },
+            () => {
+              this.$message.error("提交失败");
+            }
+          ).finally(()=>{
+            this.btnLoading = false;
+          })
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/router/adviceDelivery.js
@@ -0,0 +1,8 @@
+import asyncComponent from "@/common/until/asyncComponent";
+export default [
+  {
+    name: "adviceDelivery",
+    path: "/adviceDelivery",
+    component: asyncComponent("adviceDelivery/adviceDelivery"),
+  }
+];
