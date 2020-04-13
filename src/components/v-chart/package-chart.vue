321512715dcc50ec316d1e7a877578dd189524cb
hu <337502348@qq.com>
2020-04-13 22:07:52 +0800

    提交

--- src/components/v-chart/package-chart.vue
+++ src/components/v-chart/package-chart.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <ve-line
-      v-if="show"
       :data="chartData"
       :extend="chartExtend"
       :settings="chartSettings"
@@ -37,22 +36,8 @@ export default {
     return {
       show: false,
       chartData: {
-        // "日期", "访问用户", "下单用户"
         columns: [],
-        rows: [
-          // { 日期: "2018-01-01", 访问用户: 1393, 下单用户: 1093 },
-          // { 日期: "2018-01-02", 访问用户: 3530, 下单用户: 3230 },
-          // { 日期: "2018-01-03", 访问用户: 2923, 下单用户: 2623 },
-          // { 日期: "2018-01-05", 访问用户: 1723, 下单用户: 1423 },
-          // { 日期: "2018-01-10", 访问用户: 3792, 下单用户: 3492 },
-          // { 日期: "2018-01-18", 访问用户: 4593, 下单用户: 4293 },
-          // { 日期: "2018-01-19", 访问用户: 4593, 下单用户: 4293 },
-          // { 日期: "2018-01-20", 访问用户: 4593, 下单用户: 4293 },
-          // { 日期: "2018-01-21", 访问用户: 4593, 下单用户: 4293 },
-          // { 日期: "2018-01-22", 访问用户: 4593, 下单用户: 4293 },
-          // { 日期: "2018-01-23", 访问用户: 4593, 下单用户: 4293 },
-          // { 日期: "2018-01-24", 访问用户: 4593, 下单用户: 4293 }
-        ]
+        rows: []
       }
     };
   },
@@ -64,23 +49,30 @@ export default {
     }).then(res => {
       let { data } = res;
       let chartData = {
-        columns: ["date"],
+        columns: [
+          "date",
+          "BuySomething-Wowcher-英國-銷售額",
+          "MagicTrend-Wowcher-英國-銷售額",
+          "歷史最高",
+          "歷史平均"
+        ],
         rows: []
       };
       data.forEach(item => {
-        let { data: dataItem } = item;
-        chartData.columns.push(item.name);
-        dataItem.forEach(itemc => {
-          let obj = {
-            date: itemc.x,
-            [item.name]: itemc.y
-          };
-          chartData.rows.push(obj);
+        let { xAxis, ydata } = item;
+        let obj = {
+          date: xAxis,
+          "BuySomething-Wowcher-英國-銷售額": "",
+          "MagicTrend-Wowcher-英國-銷售額": "",
+          歷史最高: "",
+          歷史平均: ""
+        };
+        ydata.forEach(({ name, value }) => {
+          obj[name] = value;
         });
+        chartData.rows.push(obj);
       });
       this.chartData = chartData;
-      this.show = true;
-      console.log(this.chartData, 22);
     });
   }
 };
