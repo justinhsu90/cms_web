074e0fb2e11361f75189c0e35dc8c5643e035cfe
hu <337502348@qq.com>
2020-05-19 22:01:12 +0800

    提交一波

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,51 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
+<script>
+import lget from 'lodash/get'
+export default {
+  created(){
+      axios({
+        url: "/dashboard/notification/list  ",
+        method: "post",
+        data: {}
+      }).then((res = []) => {
+        res.forEach((item, index) => {
+          let msg = lget(item, 'msg', '')
+          let id = lget(item, 'dealId', '')
+            this.$message({
+            customClass: `notice-${index}`,
+            showClose: true,
+            message: msg,
+            type: 'info',
+            duration: 0,
+            onClose: () => {
+              this.noticeServe(id)
+            }
+          });
+        });
+      });
+  },
+  methods: {
+    noticeServe(id){
+       axios({
+        url: "/dashboard/notification/markasseen",
+        method: "post",
+        data: {
+          dealId: id
+        } 
+      }).then(() => {});
+    }
+  }
+}
+</script>
 <style>
+.notice-1 {
+  top: 80px;
+}
+.notice-2 {
+  top: 140px;
+}
 </style>
