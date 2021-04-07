7b66adb437e9452e65145a8bc370981eb557ac0c
hu <337502348@qq.com>
2021-04-07 21:33:52 +0800

    增加dialog

---
+++ src/components/sku/showSuccess.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="info-success">
+      <img
+        width="100%"
+        src="@/assets/img/gou.jpg"
+        alt=""
+      >
+    </div>
+    <div class="info-tips">xxxxxx</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.info-success {
+  width: 100%;
+  background: #ade1cd;
+  padding: 40px;
+  box-sizing: border-box;
+}
+.info-tips {
+  text-align: center;
+  color: #666;
+  font-size: 20px;
+}
+</style>
